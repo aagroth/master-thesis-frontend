db6cb921135ed6225b5c9c353632a9ffa3831a26
[adam] <92029070+aagroth@users.noreply.github.com>
2022-12-22 17:52:05 +0100

    Added new imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
+import AboutUs from '@/views/AboutUs.vue'
+import Cart from '@/views/Cart.vue'
+import Checkout from '@/views/Checkout.vue'
+import Product from '@/views/Product.vue'
+import Products from '@/views/Products.vue'
+import TermsAndCondition from '@/views/TermsAndCondition.vue'
+import User from '@/views/User.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
