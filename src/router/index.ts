328f6a9d7fd192cbe6322ed8bd116a44867d11e8
[adam] <92029070+aagroth@users.noreply.github.com>
2022-12-22 17:52:19 +0100

    Removed old routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,15 +14,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
