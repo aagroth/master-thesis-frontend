5b642d19ed06e219a0095a3b6039589abd2c7805
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-04 23:15:55 +0100

    Added padding bottom in Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="columns">
     <div class="column is-three-fifths is-offset-one-fifth">
-      <h1 class="is-size-3">Do you want to contact me?</h1>
+      <h1 class="is-size-3 pb-2">Do you want to contact me?</h1>
       <p>If you have any questions regarding my merch please contact me, using the contact form below!</p>
     </div>
   </div>
