1971d9ec8f9f63c95d9b4c3c95351173ccec72be
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-04 19:04:11 +0100

    Fixed dummie text for now

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-  <footer class="footer">
+  <footer class="footer has-background-dark">
     <div class="columns">
-      <div class="column is-half">
-        <p>A magic merch store!</p>
+      <div class="column is-half has-text-centered">
+        <h3 class="is-size-4 has-text-white-bis">A magic merch store!</h3>
       </div>
-      <div class="column auto">
-        
+      <div class="column auto has-text-centered">
+        <h3 class="is-size-4 has-text-white-bis">Instagram</h3>
       </div>
     </div>
   </footer>
