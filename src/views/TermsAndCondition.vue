d2286aac9421cc0ddf6363c20905be20081a6ba4
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-04 23:08:37 +0100

    Added template for terms and condition

--- src/views/TermsAndCondition.vue
+++ src/views/TermsAndCondition.vue
@@ -1,3 +1,22 @@
 <template>
-    This is the terms and condition page
+  <div class="columns">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      <h1 class="is-size-3 pb-2">Terms and Conditions</h1>
+      <p class="pb-2">Welcome to Groth merchandise!</p>
+      <p class="pb-2">These terms and conditions outline the rules and regulations for the use of Company Groth's Website.</p>
+      <p class="pb-2">
+        By accessing this website we assume you accept these terms and conditions. Do not continue to use Groth 
+        merchandise if you do not agree to take all of the terms and conditions stated on this page.
+      </p>
+      <p class="pb-2">
+        The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: “Client”, “You” and “Your” refers to you, 
+        the person log on this website and compliant to the Company's terms and conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”, 
+        “Parties”, or “Us”, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process 
+        of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, 
+        in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, 
+        are taken as interchangeable and therefore as referring to same.
+      </p>
+      <p>...</p>
+    </div>
+  </div>
 </template>
