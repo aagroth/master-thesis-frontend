692ede93d8f44a258b15d599a57a508a175ddbcf
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-04 18:29:27 +0100

    Removed old code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,12 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        A magic merch store!
-      </p>
+    <div class="columns">
+      <div class="column is-half">
+        <p>A magic merch store!</p>
+      </div>
+      <div class="column auto">
+        
+      </div>
     </div>
   </footer>
 </template>
