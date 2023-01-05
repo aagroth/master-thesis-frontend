f6bfa5bbdd5d74e5e44fdcd0dcc43deab8841b7e
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-05 21:37:12 +0100

    Added heroes for home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,32 @@
 <template>
-    This is the home page
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <p class="title">
+        Primary hero
+      </p>
+      <p class="subtitle">
+        Primary subtitle
+      </p>
+    </div>
+  </section>
+  <section class="hero is-medium is-link">
+    <div class="hero-body">
+      <p class="title">
+        Medium hero
+      </p>
+      <p class="subtitle">
+        Medium subtitle
+      </p>
+    </div>
+  </section>
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <p class="title">
+        Contact me!
+      </p>
+      <p class="subtitle">
+        Use the contact form
+      </p>
+    </div>
+  </section>
 </template>
