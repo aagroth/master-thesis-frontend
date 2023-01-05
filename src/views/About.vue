e4a48c7bf6c649d61cd5dd0dc3c97bd2fa7709f7
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-05 11:50:04 +0100

    Added template code for About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,20 @@
 <template>
-    This is the about us page
+  <div class="columns">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      <h1 class="is-size-3 pb-2">About Groth's merchandise</h1>
+      <p class="pb-2">This is a webshop dedicated to Adam's own merch. Here you can find a large number of products.</p>
+      <p class="pb-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
 </template>
