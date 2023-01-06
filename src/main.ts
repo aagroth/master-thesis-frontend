da0c5822666e708112852bc5c428a189dcdd397f
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-06 14:24:15 +0100

    Installed font awesome to the project

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,22 @@ import router from './router'
 
 import './assets/main.scss'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faBagShopping)
 
 const app = createApp(App)
+
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
