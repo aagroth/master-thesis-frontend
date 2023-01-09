ece9199bc6bf29f624ddd2fc28e55c3786280479
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-09 23:13:51 +0100

    Added getProducts function
    Also added data properties and mounted

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,3 +1,27 @@
 <template>
     This is the product page
-</template>
+</template>  export default {
+      data () {
+        return {
+          idProduct: this.$route.params.id,
+        }
+      },
+      mounted: function () {
+        this.getProducts()
+      },
+      methods: {
+        getProducts: function () {
+          fetch('https://fakestoreapi.com/products')
+          .then(res=>res.json())
+          .then(json => {
+            console.log(json)
+            this.products = json
+            console.log(this.products)
+          })
+          .catch(error => {
+            console.log('Something went wrong!')
+          })
+        }
+      }
+    }
+</script>
