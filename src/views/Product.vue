8be07e9c78fef1de93c4c76ffafdaa5155b2d02c
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-09 23:14:25 +0100

    Added template design for single product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,45 @@
 <template>
-    This is the product page
-</template>  export default {
+  <div class="" v-for="(product,index) in products" :key="index">
+    <div class="container" v-if="idProduct == product.id">
+      <div class="columns">
+        <div class="column is-half">
+          <figure class="image">
+            <img :src="product.image" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="column auto">
+          <table class="table">
+            <thead>
+              <th>Details</th>
+              <th></th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Title:</td>
+                <td>{{ product.title }}</td>
+              </tr>
+              <tr>
+                <td>Description:</td>
+                <td>{{ product.description }}</td>
+              </tr>
+              <tr>
+                <td>Price:</td>
+                <td>{{ product.price }}</td>
+              </tr>
+              <tr>
+                <td>Category:</td>
+                <td>{{ product.category }}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button class="button is-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
       data () {
         return {
           idProduct: this.$route.params.id,
