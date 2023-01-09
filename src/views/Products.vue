248d2ac5b1bbe09969c286fb79a0d343a6033237
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-09 19:26:31 +0100

    Added function to get test products

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,3 +1,18 @@
 <template>
     This is the products page
-</template>
+</template>      getProducts: function () {
+        console.log('We got here!')
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json => {
+          console.log(json)
+          this.products = json
+          console.log(this.products)
+        })
+        .catch(error => {
+          console.log('Something went wrong!')
+        })
+      }
+    }
+  }
+</script>
