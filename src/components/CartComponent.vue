fa8cf25ab5b6fa3405ce47de38f07f09d86027f0
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-10 17:41:03 +0100

    Added functions and properties

---
+++ src/components/CartComponent.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'CartComponent',
+  emits: ['closeCartComponent'],
+  data () {
+    return {
+      cart: []
+    }
+  },
+  mounted: function () {
+    this.getProductsFromLocalStorage()
+  },
+  methods: {
+    getProductsFromLocalStorage: function () {
+      this.cart = JSON.parse(localStorage.getItem("cart"))
+    },
+    removeProductFromLocalStorage: function (idProduct) {
+      // Add way to remove specific product from LocalStorage
+    }
+  }
+}
+</script>
