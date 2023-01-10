39f708b10538f08fe909241d308a34d4861c61b0
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-10 22:50:34 +0100

    Added properties for script

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -3,6 +3,28 @@
 </template>
 <script>
   export default {
-    name: 'Checkout'
+    name: 'Checkout',
+    data () {
+      return {
+        cart: []
+      }
+    },
+    mounted: function () {
+      this.getProductsFromLocalStorage()
+    },
+    computed: {
+    totalSum(){
+      let total = 0;
+      this.cart.forEach((item, i) => {
+          total += item.price
+      });
+      return total;
+    }
+    },
+    methods: {
+      getProductsFromLocalStorage: function () {
+      this.cart = JSON.parse(localStorage.getItem("cart"))
+      }
+    }
   }
 </script>
