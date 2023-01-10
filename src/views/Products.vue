080549a35a93b1816f708984d903d3406ec1be37
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-10 15:44:30 +0100

    Removed console logs

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -43,13 +43,10 @@
     },
     methods: {
       getProducts: function () {
-        console.log('We got here!')
         fetch('https://fakestoreapi.com/products')
         .then(res=>res.json())
         .then(json => {
-          console.log(json)
           this.products = json
-          console.log(this.products)
         })
         .catch(error => {
           console.log('Something went wrong!')
