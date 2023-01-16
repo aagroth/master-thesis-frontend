1b5a95d31eecaa11da7d115b6087d36955ad8b28
[adam] <92029070+aagroth@users.noreply.github.com>
2023-01-16 15:40:03 +0100

    Removed console log

--- src/components/apiManager.ts
+++ src/components/apiManager.ts
@@ -2,7 +2,6 @@ export default {
   name: 'apiManager',
 
   getAllProducts: function () {
-    console.log('we got here')
     return new Promise((resolve, reject) => {
       fetch('http://localhost:3000/master-thesis-backend')
       .then(res=>res.json())
